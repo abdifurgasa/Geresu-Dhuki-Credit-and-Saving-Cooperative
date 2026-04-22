1ba802dcc4fa13c7624bcb027873f0f5d282e6fb
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-04-22 13:33:44 +0300

    Initialize Firebase app with authentication and Firestore

---
+++ js/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS-3e_WqGHNycDgvlXVkInaynTnvnplYE",
+  authDomain: "geresu-dhuki-sacco.firebaseapp.com",
+  projectId: "geresu-dhuki-sacco",
+  storageBucket: "geresu-dhuki-sacco.firebasestorage.app",
+  messagingSenderId: "944934938425",
+  appId: "1:944934938425:web:caef23f2f3bb34c843eae8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
