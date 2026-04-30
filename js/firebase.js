2913dd1370071782c8ea95d19438f843e902e8a8
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-04-30 09:58:32 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,6 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+/* =========================
+   FIREBASE CONFIG
+========================= */
 const firebaseConfig = {
   apiKey: "AIzaSyCS-3e_WqGHNycDgvlXVkInaynTnvnplYE",
   authDomain: "geresu-dhuki-sacco.firebaseapp.com",
@@ -11,7 +14,13 @@ const firebaseConfig = {
   appId: "1:944934938425:web:caef23f2f3bb34c843eae8"
 };
 
+/* =========================
+   INIT APP
+========================= */
 const app = initializeApp(firebaseConfig);
 
+/* =========================
+   EXPORT SERVICES
+========================= */
 export const auth = getAuth(app);
 export const db = getFirestore(app);
