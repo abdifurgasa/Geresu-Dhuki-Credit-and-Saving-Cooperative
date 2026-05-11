8c00d97c221139ed3a65a4462ab3df0b4916a789
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-05-11 15:51:35 +0300

    Create am.js

---
+++ lang/am.js
@@ -0,0 +1,20 @@
+export default {
+
+  dashboard: "ዳሽቦርድ",
+  members: "አባላት",
+  savings: "ቁጠባ",
+  loans: "ብድር",
+  reports: "ሪፖርቶች",
+  settings: "ቅንብሮች",
+  logout: "ውጣ",
+  totalMembers: "ጠቅላላ አባላት",
+  totalSavings: "ጠቅላላ ቁጠባ",
+  totalLoans: "ጠቅላላ ብድር",
+  totalProfit: "ጠቅላላ ትርፍ",
+  addMember: "አባል ጨምር",
+  createLoan: "ብድር ፍጠር",
+  depositSavings: "ቁጠባ አስገባ",
+  financialAnalytics: "ፋይናንስ ትንተና",
+  welcome: "ወደ SACCO ስርዓት እንኳን በደህና መጡ"
+
+};
