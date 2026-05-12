94a0870bcabf71ede5dc5e135a3c6c7fb7738070
abdifurgasa <abdifurgasa2nd@gmail.com>
2026-05-12 20:07:02 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 /* =========================
    FIREBASE CONFIG
@@ -20,7 +21,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 /* =========================
-   EXPORT SERVICES
+   SERVICES
 ========================= */
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);   // ✅ IMPORTANT FIX
+export { app };
